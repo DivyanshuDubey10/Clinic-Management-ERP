d7f7fa9bddabf6c25c7ab2a59c823031fa5c4a2f
Divyanshu <divyadubey2206@gmail.com>
2026-07-30 20:37:00 +0530

    Fix middleware import typo

--- backend/routes/notificationRoutes.js
+++ backend/routes/notificationRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { getNotifications, markAsRead, markAllAsRead } = require('../controllers/notificationController');
-const { protect } = require('../middleware/authMiddleware');
+const { protect } = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
